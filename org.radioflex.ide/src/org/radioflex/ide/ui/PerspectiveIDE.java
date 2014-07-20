07efd58459a9e823ed4859fb34458621f2bafa9f
YMLiang <609971367@qq.com>
2014-07-20 10:15:01 +0800

    透视图
    Signed-off-by: YMLiang <609971367@qq.com>

---
+++ org.radioflex.ide/src/org/radioflex/ide/ui/PerspectiveIDE.java
@@ -0,0 +1,13 @@
+package org.radioflex.ide.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveIDE implements IPerspectiveFactory {
+		
+		
+	    public void createInitialLayout(IPageLayout layout) {
+	    	layout.addView("org.radioflex.ide.projectview", IPageLayout.LEFT, 0.4f, IPageLayout.ID_EDITOR_AREA);
+	    }
+	}
+
